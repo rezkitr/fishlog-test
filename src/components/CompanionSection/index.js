9d2e523652bccb209a7c652254c1ad325516473c
Rezki Triandy <rezki.triandy@majoo.id>
2023-08-02 08:03:42 +0700

    update image

--- src/components/CompanionSection/index.js
+++ src/components/CompanionSection/index.js
@@ -11,7 +11,7 @@ const CompanionSection = () => {
                     <SlideItem imageSrc="https://www.logo.wine/a/logo/Coca-Cola/Coca-Cola-Logo.wine.svg" />
                     <SlideItem imageSrc="https://purepng.com/public/uploads/large/purepng.com-ebay-logologobrand-logoiconslogos-251519938326w1a5t.png" />
                     <SlideItem imageSrc="https://purepng.com/public/uploads/large/purepng.com-fedex-logologobrand-logoiconslogos-251519939539h7rji.png" />
-                    <SlideItem imageSrc="https://assets.stickpng.com/images/5cb77367a7c7755bf004c0f4.png" />
+                    <SlideItem imageSrc="https://purepng.com/public/uploads/large/purepng.com-walmart-logologobrand-logoiconslogos-251519939045oqelv.png" />
                     <SlideItem imageSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Nabati_brand_logo.svg/2560px-Nabati_brand_logo.svg.png" />
                     <SlideItem imageSrc="https://1000logos.net/wp-content/uploads/2017/03/Levis-Logo.png" />
                     <SlideItem imageSrc="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png" />
